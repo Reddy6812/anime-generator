88ba9ac127f30b9f8beb972a2be41df716a98dc1
Vijay Reddy <vijayreddy6812@gmail.com>
2025-03-30 00:41:45 -0400

    Create dataset.py

---
+++ utils/dataset.py
@@ -0,0 +1,20 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+class AnimeDataset(Dataset):
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+        self.image_paths = [os.path.join(root_dir, f) for f in os.listdir(root_dir)]
+        self.transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.image_paths[idx])
+        return self.transform(img)
